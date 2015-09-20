0df40caccb4a0f1266d9b415ea2d74364d6622cc
Syntactic Vexation <syntacticvexation@gmail.com>
2015-09-20 22:17:09 +1000

    added packer files and README

---
+++ README.md
@@ -0,0 +1,20 @@
+# Kali 2.0 Packer Vagrant Box
+
+A basic Packer definition for Kali Linux 2.0 to build a Vagrant box.
+
+## Requirements
+
+The following software must be installed/present on your local machine before you can use Packer to build the Vagrant box file:
+
+  - [Packer](http://www.packer.io/)
+  - [Vagrant](http://vagrantup.com/)
+  - [VirtualBox](https://www.virtualbox.org/)
+
+## Usage
+
+    $ packer build template.json
+
+## License
+
+GPLv3.
+
---
+++ script/base.sh
@@ -0,0 +1,28 @@
+# Update the box
+apt-get -y update
+# we want to kill any 'helpful' messages letting us know how the package has been updated
+DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt-get -q -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
+#DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
+apt-get -y install linux-headers-$(uname -r) build-essential
+apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libyaml-0-2
+apt-get -y install curl unzip vim
+
+# Set up sudo
+echo 'vagrant ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/vagrant
+
+# Tweak sshd to prevent DNS resolution (speed up logins)
+echo 'UseDNS no' >> /etc/ssh/sshd_config
+
+# Remove 5s grub timeout to speed up booting
+cat <<EOF > /etc/default/grub
+# If you change this file, run 'update-grub' afterwards to update
+# /boot/grub/grub.cfg.
+
+GRUB_DEFAULT=0
+GRUB_TIMEOUT=0
+GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_CMDLINE_LINUX_DEFAULT="quiet"
+GRUB_CMDLINE_LINUX="debian-installer=en_US"
+EOF
+
+update-grub
